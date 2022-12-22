f97e6da9f1be2e2e8c361cc7127003d4b8adfe06
bobytudu2000 <boby.tudu@nirmitee.io>
2022-12-22 15:26:30 +0530

    hello

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ app.use(express.static(__dirname + "/public"));
 
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
 
 
 
