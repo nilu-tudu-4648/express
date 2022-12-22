7117880aa55f90d7a670cbfa0be39df52f82a294
bobytudu2000 <boby.tudu@nirmitee.io>
2022-12-22 13:34:04 +0530

    static

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-  });
+
+app.use("/public", express.static(__dirname + "/public"));
+// app.get("/", function(req, res) {
+//     res.sendFile(__dirname + "/public");
+//   });
 
 
 
