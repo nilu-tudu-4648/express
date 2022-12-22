3730e294ab51354441cd999f947d1511fb3ca745
bobytudu2000 <boby.tudu@nirmitee.io>
2022-12-22 13:13:24 +0530

    dir

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
 
-
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/public/index.html");
   });
 
 
