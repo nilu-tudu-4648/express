21a27006160bc9f5bc657e5f0e3b153665c3c929
bobytudu2000 <boby.tudu@nirmitee.io>
2022-12-22 15:56:05 +0530

    upper

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config()
 
 // Normal usage
 app.use(express.static(__dirname + "/public"));
@@ -10,9 +10,15 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", (req, res) => {
-    res.json({
-      message: "Hello json"
-    });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({
+            message: "Hello json".toUpperCase()
+          })
+      } else {
+        res.json({
+            message: "Hello json"
+          })
+      }
   });
 
 
