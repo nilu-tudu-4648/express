2cb91e6ea48f8b1d46f5b72bf6f28e61b1758b21
bobytudu2000 <boby.tudu@nirmitee.io>
2022-12-22 16:23:23 +0530

    rem

--- myApp.js
+++ myApp.js
@@ -3,12 +3,6 @@ let app = express();
 const dotenv = require('dotenv')
 dotenv.config()
 
-// Normal usage
-app.use(express.static(__dirname + "/public"));
-
-// Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
-
 
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE === "uppercase") {
