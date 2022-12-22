b4b9391777ce33c7236749c61b009c6e2f131160
bobytudu2000 <boby.tudu@nirmitee.io>
2022-12-22 15:49:30 +0530

    both

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 
 app.get("/json", (req, res) => {
     res.json({
